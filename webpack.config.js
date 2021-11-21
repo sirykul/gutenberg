976c5aa1e9338e54960e040fa7c5c965e76896a6
Kai Hao <kevin830726@gmail.com>
2021-11-22 01:33:28 +0800

    Add experimental dev-only webpack single pass build

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,8 @@
  */
 const blocksConfig = require( './tools/webpack/blocks' );
 const packagesConfig = require( './tools/webpack/packages' );
+const experimentalConfig = require( './tools/webpack/experimental.config' );
 
-module.exports = [ blocksConfig, packagesConfig ];
+module.exports = process.env.EXPERIMENTAL_BUILD
+	? experimentalConfig
+	: [ blocksConfig, packagesConfig ];
